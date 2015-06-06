424e96adff02c267e50fbdef8ac3fd4645423870
MiklosNovak <EMike111@gmail.com>
2015-06-06 19:06:49 -0400

    Calculator test project

---
+++ Calculator/Calculator.Test/CalculatorTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Test
+{
+	[TestClass]
+    public class CalculatorTests
+	{
+		Calculator _calculator;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			_calculator = new Calculator();
+		}
+
+		[TestMethod]
+		public void TestCalculatorInitialization()
+		{
+			Assert.AreEqual(0, _calculator.Value);
+		}
+
+		[TestMethod]
+		public void TestCalculatorAddition()
+		{
+			_calculator.Add(99);
+
+			Assert.AreEqual(99, _calculator.Value);
+		}
+
+		[TestMethod]
+		public void TestCalculatorAdditionWith2Number()
+		{
+			_calculator.Add(3);
+			_calculator.Add(5);
+
+			Assert.AreEqual(8, _calculator.Value);
+		}
+
+		[TestMethod]
+		public void TestCalculatorReset()
+		{
+			_calculator.Add(3);			
+			_calculator.Reset();
+			Assert.AreEqual(0, _calculator.Value);
+		}
+    }
+}
---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+	    int _value;
+
+		public int Value 
+		{ 
+			get { return _value; } 
+		}
+
+	    public Calculator()
+	    {
+		    Reset();
+	    }
+
+	    public void Add(int x)
+	    {
+		    _value += x;
+	    }
+
+	    public void Reset()
+	    {
+		    _value = 0;
+	    }
+    }
+}
